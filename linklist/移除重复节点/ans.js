81492552b279c630e6e76e596f169b60c45b3a7e
Vibes-INS <j893412899@outlook.com>
2020-03-08 16:31:26 +0800

    :zap: code normalization

--- linklist/移除重复节点/ans.js
+++ linklist/移除重复节点/ans.js
@@ -9,17 +9,17 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var removeDuplicateNodes = function(head) {
+var removeDuplicateNodes = function (head) {
   let ans = new ListNode()
   let ans_pointer = ans
   const map = {}
 
   for (let p = head; p !== null; p = p.next) {
-      if (map[p.val] === undefined) {
-          map[p.val] = true
-          ans_pointer.next = new ListNode(p.val)
-          ans_pointer = ans_pointer.next
-      }
+    if (map[p.val] === undefined) {
+      map[p.val] = true
+      ans_pointer.next = new ListNode(p.val)
+      ans_pointer = ans_pointer.next
+    }
   }
 
   return ans.next
