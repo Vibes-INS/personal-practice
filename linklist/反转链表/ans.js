cc915e447956864719dbaebf31f41687542f3197
Vibes-INS <j893412899@outlook.com>
2020-03-08 16:07:09 +0800

    :tada: add new files

---
+++ linklist/反转链表/ans.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+  let curr = null,
+      prev = head
+  
+  while (prev) {
+      let t = prev.next
+
+      prev.next = curr
+      curr = prev
+      prev = t
+  }
+
+  return curr
+};
